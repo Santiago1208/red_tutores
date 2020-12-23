e7569e85642ffa6841cdc8caf1edf8e426950723
santiago-restrepo <sanpra1208@gmail.com>
2020-12-23 15:25:56 -0500

    "Add project structure"

---
+++ src/main/java/com/co/red_tutores/backend/RedTutoresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.co.red_tutores.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedTutoresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedTutoresBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/red_tutores/backend/RedTutoresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.red_tutores.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedTutoresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
